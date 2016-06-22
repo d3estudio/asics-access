9a2e12352195ce8dfe5d893189a16e808c094a8a
Matheus Portillo <mathportillo@gmail.com>
2016-06-22 12:04:42 -0300

    added rqrcode gem that generates qrcode html for email

--- web/app/mailers/common_mailer.rb
+++ web/app/mailers/common_mailer.rb
@@ -3,6 +3,7 @@ class CommonMailer < ApplicationMailer
 
   def confirm_email(user)
     @user = user
+    @qr = RQRCode::QRCode.new( user.qr_code, :size => 5, :level => :h )
     mail(to: @user.email, subject: 'Confirmação Asics Hub')
   end
 
