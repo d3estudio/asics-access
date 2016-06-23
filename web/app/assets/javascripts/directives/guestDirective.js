92285c43a79aaaddb2218e34419427dbfad015e8
Matheus Portillo <mathportillo@gmail.com>
2016-06-23 17:27:46 -0300

    added new guests directive

---
+++ web/app/assets/javascripts/directives/guestDirective.js
@@ -0,0 +1,30 @@
+angular.module('asics').directive('myGuest',  [
+    '$interval',
+    function($interval) {
+        function link(scope, element, attrs) {
+            // var timeoutId;
+            //
+            // function updateTime() {
+            //    console.log('funciona')
+            // }
+            //
+            // element.on('$destroy', function() {
+            //     $interval.cancel(timeoutId);
+            // });
+            //
+            // // start the UI update process; save the timeoutId for canceling
+            // timeoutId = $interval(function() {
+            //     updateTime(); // update DOM
+            // }, 1000);
+        }
+
+
+        return {
+            link: link,
+            scope: {
+                guest: '='
+            },
+            templateUrl: '/views/directives/guest'
+        };
+    }
+]);
