ff2e5768af22e9789f9035743427c6d783151de8
Matheus Portillo <mathportillo@gmail.com>
2016-07-11 19:42:25 -0300

    added passbook files

---
+++ web/config/initializers/passbook.rb
@@ -0,0 +1,16 @@
+require 'passbook'
+
+Passbook.configure do |passbook|
+
+  # Path to your wwdc cert file
+  passbook.wwdc_cert = Rails.root.join('config', 'certificates', 'wwdrca.pem')
+
+  # Path to your cert.p12 file
+  passbook.p12_certificate = Rails.root.join('config', 'certificates', 'pass_type_certificate.p12')
+
+  # Password for your certificate
+  passbook.p12_password = 
+
+  # Key for signing the file
+  # passbook.p12_key = Rails.root.join('key.pem')
+end
