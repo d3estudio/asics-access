07afcb08949af184fd52548fbc79e7203af1ca14
phillfer <philliperamos@gmail.com>
2016-07-14 00:11:17 -0300

    fixed confirmed layout page

--- web/app/assets/javascripts/controllers/rsvpConfirmedCtrl.js
+++ web/app/assets/javascripts/controllers/rsvpConfirmedCtrl.js
@@ -1,23 +1,29 @@
 angular.module('asics').controller('RsvpConfirmedCtrl', [
-    '$scope',
-    '$state',
-    '$stateParams',
-    function ($scope, $state, $stateParams) {
-        $scope.result = '';
-        $scope.strings = {};
-        $scope.guest = $stateParams.guest;
+  '$scope',
+  '$state',
+  '$stateParams',
+  function ($scope, $state, $stateParams) {
+    $scope.result = '';
+    $scope.strings = {};
+    $scope.guest = $stateParams.guest;
 
-        if ($scope.guest == null) $state.go('home');
-        else angular.copy(confirmedStrings[$scope.guest.language], $scope.strings);
+    if ($scope.guest == null) $state.go('home');
+    else angular.copy(confirmedStrings[$scope.guest.language], $scope.strings);
 
-    }
+  }
 ]);
 
 var confirmedStrings = {
-    EN: {
-        confirmedCodeMessage: "this is your checkin code for the event"
-    },
-    PT: {
-        confirmedCodeMessage: "esse é seu código para entrar no evento"
-    }
+  EN: {
+    expression: "GREAT!",
+    confirmedCodeMessage: "This code is your entry to the ASICS Hub.",
+    reminderMessage: "Make sure you bring it with you.",
+    emailMessage: "We just sent this code to your e-mail"
+  },
+  PT: {
+    expression: "ÓTIMO!",
+    confirmedCodeMessage: "Esse é seu código para entrar no ASICS Hub",
+    reminderMessage: "Certifique-se de trazê-lo com você.",
+    emailMessage: "Já enviamos o código para seu email"
+  }
 };
