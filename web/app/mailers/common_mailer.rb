c6d7c15b9421abbe549afa598f572cb2b8e314cc
phillfer <philliperamos@gmail.com>
2016-07-18 18:00:47 -0300

    added pt and en strings for invite and confirm emails

--- web/app/mailers/common_mailer.rb
+++ web/app/mailers/common_mailer.rb
@@ -46,7 +46,18 @@ class CommonMailer < ApplicationMailer
         img_03: 'http://asicshub.com.br/images/email/invitation/en/asics_03.jpg',
         img_04: 'http://asicshub.com.br/images/email/invitation/en/asics_04.jpg',
         img_05: 'http://asicshub.com.br/images/email/invitation/en/asics_05.jpg',
-        img_06: 'http://asicshub.com.br/images/email/invitation/en/asics_06.jpg'
+        img_06: 'http://asicshub.com.br/images/email/invitation/en/asics_06.jpg',
+        salutation: 'Dear GUEST',
+        intro: 'It gives us great pleasure to invite you to our ASICS Hub during the 2016 Rio Olympics.
+                The name implies exactly what it means: a hub that will bring together all the pillars that
+                make ASICS the preferred choice for dedicated athletes: our history, sports, athletes, new products,
+                design and performance. Not to mention a complete hospitality experience with fantastic food, drink,
+                entertainment and interactions.',
+        link: 'Click on the link below to confirm your attendance and to receive the access code.',
+        when: 'WHEN: AUGUST 03RD UNTIL AUGUST 21ST, FROM 12PM TO 10PM',
+        where: 'WHERE: EST. BOCA DO MATO, 803, VARGEM PEQUENA - RIO DE JANEIRO',
+        schedule: 'Learn more about our schedule here:',
+        explanation: 'You are receiving this message because you were invited to enjoy the ASICS Hub on Rio 2016 Olympics.'
       },
       PT: {
         img_01: 'http://asicshub.com.br/images/email/invitation/pt/asics_01.jpg',
@@ -54,7 +65,18 @@ class CommonMailer < ApplicationMailer
         img_03: 'http://asicshub.com.br/images/email/invitation/pt/asics_03.jpg',
         img_04: 'http://asicshub.com.br/images/email/invitation/pt/asics_04.jpg',
         img_05: 'http://asicshub.com.br/images/email/invitation/pt/asics_05.jpg',
-        img_06: 'http://asicshub.com.br/images/email/invitation/pt/asics_06.jpg'
+        img_06: 'http://asicshub.com.br/images/email/invitation/pt/asics_06.jpg',
+        salutation: 'Caro CONVIDADO',
+        intro: 'Temos o grande prazer de convidá-lo para o ASICS Hub durante as Olimpíadas Rio 2016.
+                O nome descreve exatamente vamos oferecer: um hub que vai unir todos os pilares que fazem a ASICS
+                a escolha preferida entre atletas dedicados: nossa história, esportes, atletas, novos produtos,
+                design e performance. Sem contar uma experiéncia de hospitalidade completa com comidas e bebidas
+                incríveis, entretenimento e outras interações.',
+        link: 'Clique no link abaixo para confirmar sua presença e receba o código de acesso.',
+        when: 'QUANDO: 03 DE AGOSTO A 21 DE AGOSTO, DAS 12hs ÀS 22hs',
+        where: 'ONDE: EST. BOCA DO MATO, 803, VARGEM PEQUENA - RIO DE JANEIRO',
+        schedule: 'Conheça mais sobre nossa programação:',
+        explanation: 'Você está recebendo esta mensagem por ter sido convidado(a) para desfrutar do ASICS Hub nos Jogos Olímpicos Rio 2016.'
       }
     }
 
