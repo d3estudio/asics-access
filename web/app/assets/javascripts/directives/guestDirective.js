b2553de6a6b0e90c0745dafec2d140451af60f0d
phillfer <philliperamos@gmail.com>
2016-07-18 22:18:26 -0300

    added strings for multi language suport on guest directive

--- web/app/assets/javascripts/directives/guestDirective.js
+++ web/app/assets/javascripts/directives/guestDirective.js
@@ -1,32 +1,33 @@
-angular.module('asics').directive('myGuest',  [
-    '$interval',
-    function($interval) {
-        function link(scope, element, attrs) {
-            // var timeoutId;
-            //
-            // function updateTime() {
-            //    console.log('funciona')
-            // }
-            //
-            // element.on('$destroy', function() {
-            //     $interval.cancel(timeoutId);
-            // });
-            //
-            // // start the UI update process; save the timeoutId for canceling
-            // timeoutId = $interval(function() {
-            //     updateTime(); // update DOM
-            // }, 1000);
-        }
-
-
-        return {
-            link: link,
-            scope: {
-                guest: '=myGuest',
-                resend: '&onResend',
-                delete: '&onDelete'
-            },
-            templateUrl: '/views/directives/guest'
-        };
+angular.module('asics').directive('myGuest', [
+  '$interval',
+  function ($interval) {
+    function link(scope, element, attrs) {
+      // var timeoutId;
+      //
+      // function updateTime() {
+      //    console.log('funciona')
+      // }
+      //
+      // element.on('$destroy', function() {
+      //     $interval.cancel(timeoutId);
+      // });
+      //
+      // // start the UI update process; save the timeoutId for canceling
+      // timeoutId = $interval(function() {
+      //     updateTime(); // update DOM
+      // }, 1000);
     }
+
+
+    return {
+      link: link,
+      scope: {
+        guest: '=myGuest',
+        resend: '&onResend',
+        delete: '&onDelete',
+        strings: '=strings'
+      },
+      templateUrl: '/views/directives/guest'
+    };
+  }
 ]);
