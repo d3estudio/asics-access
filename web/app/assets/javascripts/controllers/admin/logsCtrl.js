32ac64e70b02a6e250352970c8f1fb8abe5b2427
phillfer <philliperamos@gmail.com>
2016-07-18 21:57:06 -0300

    added strings for multi language suport on admin logs

--- web/app/assets/javascripts/controllers/admin/logsCtrl.js
+++ web/app/assets/javascripts/controllers/admin/logsCtrl.js
@@ -1,70 +1,88 @@
 angular.module('asics').controller('LogsCtrl', [
-    '$mdToast',
-    '$scope',
-    '$interval',
-    'admin',
-    function($mdToast, $scope, $interval, admin) {
-        $scope.searchText = '';
-        $scope.listUpdatedAt = new Date();
-        $scope.logs = [];
+  '$mdToast',
+  '$scope',
+  '$interval',
+  'admin',
+  '$stateParams',
+  function ($mdToast, $scope, $interval, admin, $stateParams) {
+    $scope.strings = {};
+    $scope.language = $stateParams.language;
+    $scope.searchText = '';
+    $scope.listUpdatedAt = new Date();
+    $scope.logs = [];
 
-        var searchTimeout;
-        var searchInterval;
+    angular.copy(adminLogsStrings[$scope.language], $scope.strings);
 
-        angular.copy(admin.logs, $scope.logs);
-        updateLogs();
+    var searchTimeout;
+    var searchInterval;
 
-        function updateLogs() {
-            admin.getLogs()
-                .then(readLogs)
-                .catch(errorToast);
-        }
+    angular.copy(admin.logs, $scope.logs);
+    updateLogs();
 
-        function readLogs() {
-            angular.copy(admin.logs, $scope.logs);
-            $scope.listUpdatedAt = new Date();
-        }
-
-        function applySearch() {
-            admin.postSearchLogs($scope.searchText)
-                .then(readSearchLogs)
-                .catch(errorToast);
-        }
-
-        function readSearchLogs(result) {
-            angular.copy(result.logs, $scope.logs);
-        }
-
-        function errorToast(error) {
-            var toast = $mdToast.simple()
-                .textContent(error)
-                .position('top right')
-                .hideDelay(3000)
-                .theme('error-toast');
-
-            $mdToast.show(toast);
-        }
-
-        function stopInterval() {
-            $interval.cancel(searchInterval);
-        }
-
-        $scope.$watch('searchText', function() {
-            clearTimeout(searchTimeout);
-            if ($scope.searchText)
-                searchTimeout = setTimeout(applySearch, 400)
-            else {
-                readLogs();
-            }
-        });
-
-        $scope.$on('$destroy', function() {
-            stopInterval();
-        });
-
-        stopInterval();
-        searchInterval = $interval(function() {
-            updateLogs();
-        }, 45 * 1000);
+    function updateLogs() {
+      admin.getLogs()
+        .then(readLogs)
+        .catch(errorToast);
     }
+
+    function readLogs() {
+      angular.copy(admin.logs, $scope.logs);
+      $scope.listUpdatedAt = new Date();
+    }
+
+    function applySearch() {
+      admin.postSearchLogs($scope.searchText)
+        .then(readSearchLogs)
+        .catch(errorToast);
+    }
+
+    function readSearchLogs(result) {
+      angular.copy(result.logs, $scope.logs);
+    }
+
+    function errorToast(error) {
+      var toast = $mdToast.simple()
+        .textContent(error)
+        .position('top right')
+        .hideDelay(3000)
+        .theme('error-toast');
+
+      $mdToast.show(toast);
+    }
+
+    function stopInterval() {
+      $interval.cancel(searchInterval);
+    }
+
+    $scope.$watch('searchText', function () {
+      clearTimeout(searchTimeout);
+      if ($scope.searchText)
+        searchTimeout = setTimeout(applySearch, 400);
+      else {
+        readLogs();
+      }
+    });
+
+    $scope.$on('$destroy', function () {
+      stopInterval();
+    });
+
+    stopInterval();
+    searchInterval = $interval(function () {
+      updateLogs();
+    }, 45 * 1000);
+  }
 ]);
+
+var adminLogsStrings = {
+  EN: {
+    updatedAt: "Last update at",
+    noCheckin: "No one has done checkin today",
+    noneFound: "No guest found"
+  },
+  PT: {
+    updatedAt: "Atualizado às",
+    noCheckin: "Nenhum convidado fez check-in hoje",
+    noneFound: "Nenhum convidado encontrado"
+  }
+};
