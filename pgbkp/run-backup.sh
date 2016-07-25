7a2c6e52ffc55f4f935aa4d692b6a827adf36002
Matheus Portillo <mathportillo@gmail.com>
2016-07-25 16:44:46 -0300

    fixed auto backup cron errors and console output

--- pgbkp/run-backup.sh
+++ pgbkp/run-backup.sh
@@ -1,20 +1,24 @@
-# Credit for lots of this goes to: https://github.com/siomiz/PostgreSQL-S3
-set -e # stop if any of these commands fail
-source /tmp/env.sh
+#!/bin/sh
 
-echo "*** Starting run-backup.sh ***"
+# stop if any of these commands fail
+set -e
+
+# logs results of this script to stdout and to file
+exec &> >(tee -a /code/cron.log)
+
+echo "Starting Postgres backup procedure"
+
+# source all env from temp file created by start.sh
+source /tmp/env.sh
 
 DATE=$(date +%Y%m%d_%H%M%S)
 PGDUMPFILE="/tmp/$PREFIX-$DATE.sql"
-S3_URI="s3://$S3_BUCKET_NAME/$PREFIX-$DATE.sql"
+S3_URI="s3://$S3_BUCKET_NAME/"
 
-echo "> Running pg_dumpall with user $POSTGRES_USER"
+echo "Running pg_dumpall with user $POSTGRES_USER"
 PGPASSWORD="$POSTGRES_PASSWORD" pg_dumpall -h postgres -U "$POSTGRES_USER" > $PGDUMPFILE
 
-echo "> Uploading to S3"
-aws s3 cp "$PGDUMPFILE" "$S3_URI"
-
-# Clean up
-rm $PGDUMPFILE
+echo "Uploading to S3"
+/usr/local/bin/aws s3 mv "$PGDUMPFILE" "$S3_URI"
 
 echo "Done."
